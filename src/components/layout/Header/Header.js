dd8fde8d23233448b4fda74efeb981cd934f6255
klaryon <klary.ardon@gmail.com>
2021-02-09 17:41:54 +0100

    header added

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom"
-import StyledHeader from "./styles"
+import { StyledHeader, StyledLink, StyledParagraph }from "./styles"
+import Cart from "../../units/Cart/Cart"
 
 const Header = () => {
     return (
-        <StyledHeader>
-            <Link to="/">Products</Link>
-            <Link to="/shop">Shop</Link>
-        </StyledHeader>
+        <>
+            <StyledHeader>
+                <h1>🍵🍓☕ 3 item store</h1>
+                <StyledLink>
+                    <Link to="/">Products</Link>
+                    <Link to="/shop">Shop<Cart/></Link>
+                </StyledLink>
+            </StyledHeader>
+            <StyledParagraph>
+                This is a store of only 3 items... hope you don't get lost. All inspiration is from our C*X's tastes and special rewards!
+            </StyledParagraph>
+        </>
     )
 }
 
--- src/components/layout/Header/styles.js
+++ src/components/layout/Header/styles.js
@@ -1,7 +1,30 @@
-import styled from "styled-components"
+import styled from "styled-components";
+import Colors from "../../../theme/Colors"
 
-const StyledHeader = styled.header`
-    background-color: blueviolet;
-`
-
-export default StyledHeader;
+export const StyledHeader = styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-left: 4rem;
+    padding-right: 4rem;
+    padding-top: 1.5rem;
+`;
+export const StyledLink = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 12rem;
+    
+    & > a {
+        display: flex;
+        align-items: center;
+        text-decoration: none;
+        color: ${Colors.black};
+    }
+`;
+export const StyledParagraph = styled.p`
+    font-style: italic;
+    padding-left: 4rem;
+    padding-top: 0rem;
+    padding-bottom: 0.5rem;
+`;
