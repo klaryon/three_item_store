bbdb5fcb8c40bd38c033053e8f22b8652e93508a
klaryon <klary.ardon@gmail.com>
2021-02-09 19:42:25 +0100

    data.js

---
+++ src/shared/data.js
@@ -0,0 +1,29 @@
+import greentea from "../assets/images/greentea.jpg"
+import strawberries from "../assets/images/strawberries.jpg"
+import coffee from "../assets/images/coffee.jpg"
+
+const data = [
+    {
+        id: "GR1",
+        name: "Green Tea",
+        image: greentea,
+        price: 3.11,
+        description: "Buy one get one free!"
+    },
+    {
+        id: "SR1",
+        name: "Strawberries",
+        image: strawberries,
+        price: 5.00,
+        description: "Buy 3 at original price, then get €0.50 discount per unit"
+    },
+    {
+        id: "CF1",
+        name: "Coffee",
+        image: coffee,
+        price: 11.23,
+        description: "Buy 3, and all your items get €3.74 discount per unit"
+    },
+];
+
+export default data;
